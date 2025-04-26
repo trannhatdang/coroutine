e4f8e4d58233ffcb838b7c2835d83a23c330d73f
trannhatdang <hihihibly@gmail.com>
2025-04-26 13:46:20 +0700

    lmao

--- Assets/Scripts/Arms.cs
+++ Assets/Scripts/Arms.cs
@@ -5,13 +5,16 @@ public class Arms : MonoBehaviour
 {
 	Animator anim;
 	SpriteRenderer spr;
+	Collider2D coll;
 	bool flag = false;
 	
 	void Start()
 	{
 		anim = GetComponent<Animator>();
 		spr = GetComponent<SpriteRenderer>();
+		coll = GetComponent<Collider2D>();
 		spr.enabled = false;
+		coll.enabled = false;
 	}
 	public IEnumerator Attack()
 	{
@@ -19,11 +22,14 @@ public class Arms : MonoBehaviour
 
 		anim.SetBool("Attack", true);
 		spr.enabled = true;
+		coll.enabled = true;
 		flag = true;
 		for(int i = 0; i < 37; ++i) yield return null;
-		flag = false;
 		anim.SetBool("Attack", false);
 		spr.enabled = false;
+		coll.enabled = false;
+		for(int i = 0; i < 30; ++i) yield return null;
+		flag = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
